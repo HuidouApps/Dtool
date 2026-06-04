c43143fc161d2e0c37a63de7622b18b3efec38d4
huidoudour <ylmf_qquser@126.com>
2026-06-04 08:22:57 +0800

    chore(build): 升级gradle和依赖库版本，清理IDE配置文件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 android {
     namespace = "dev.huidou.util"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(37) {
+            minorApiLevel = 0
         }
     }
 
